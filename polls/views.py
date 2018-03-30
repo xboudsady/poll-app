06d1440d561fb85fd154d1e63fc75531b7754806
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-03-30 12:52:20 -0700

    Added error handling for views.py question details

--- polls/views.py
+++ polls/views.py
@@ -1,20 +1,22 @@
-# from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
 
 from .models import Question
 
 
 def index(request):
-    # return HttpResponse("Hello, world. You're at the polls index.")
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
+    try:
+        question = Question.objects.pet(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
     return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
