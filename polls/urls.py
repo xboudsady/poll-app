ec460291e2037bfadc45dea216234ad2624f6749
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-03-31 11:32:21 -0700

    Broken, need to fix view on generic template rendering

--- polls/urls.py
+++ polls/urls.py
@@ -5,10 +5,10 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     # ex: /polls/5/
     # the 'name' value is called by the {% url %} template tag
-    path('<int:pk>/', views.DetailViews.as_view(), name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # ex:/polls/5/results/
     path('<int:pk/results/', views.ResultsView.as_view(), name='results'),
     # ex:/polls/5/vote/
--- polls/views.py
+++ polls/views.py
@@ -11,7 +11,7 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published question."""
+        """Return the last five published questions."""
         return Question.objects.order_by('-pub_date')[:5]
 
 
